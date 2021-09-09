98ccd8e531b71e685dee521e2e16479644c910ed
meaghannotmegan <81677346+meaghannotmegan@users.noreply.github.com>
2021-09-09 11:06:59 -0400

    Update StitchCountForBall.py

--- StitchCountForBall.py
+++ StitchCountForBall.py
@@ -1,2 +1,6 @@
-#how to make a crochet ball
+how to make a crochet ball
+print("This is a stitch count calculator for knitting a ball.")
+diameter = int(input("What is the desired diameter of your ball? "))
+
 #math to figure out stitch count based on dimensions entered by user
+
